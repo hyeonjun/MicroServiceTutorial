d8e84b92860cb22515751425fe7a0d92ce2f4e3c
zlel175 <zlel175@naver.com>
2022-04-10 22:01:26 +0900

    Connection PostgreSQL

--- tutorialservice/customer/src/main/java/com/tutorial/customer/Customer.java
+++ tutorialservice/customer/src/main/java/com/tutorial/customer/Customer.java
@@ -1,10 +1,24 @@
 package com.tutorial.customer;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data @Builder
+import javax.persistence.*;
+
+@Data @Builder @Entity @AllArgsConstructor @NoArgsConstructor
 public class Customer {
+
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String name;
     private String nickname;
--- tutorialservice/customer/src/main/java/com/tutorial/customer/CustomerService.java
+++ tutorialservice/customer/src/main/java/com/tutorial/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.tutorial.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .name(request.name())
@@ -12,6 +12,6 @@ public record CustomerService() {
                 .build();
         // todo: check if email valid
         // todo: check if email not taken
-        // todo: store customer in db
+        customerRepository.save(customer);
     }
 }

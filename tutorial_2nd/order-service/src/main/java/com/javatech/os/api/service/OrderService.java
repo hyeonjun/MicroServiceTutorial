c4071d1e97ad771c52860bbce1c7d6ee0eaf2211
zlel175 <zlel175@naver.com>
2022-04-13 00:22:57 +0900

    Cloud Config

--- tutorial_2nd/order-service/src/main/java/com/javatech/os/api/service/OrderService.java
+++ tutorial_2nd/order-service/src/main/java/com/javatech/os/api/service/OrderService.java
@@ -7,15 +7,24 @@ import com.javatech.os.api.entity.Order;
 import com.javatech.os.api.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service @RequiredArgsConstructor
+@RefreshScope
 public class OrderService {
 
     @Autowired
     private final OrderRepository repository;
+
+    @Autowired @Lazy
     private final RestTemplate template;
+
+    @Value("${microservice.payment-service.endpoints.endpoint.uri}")
+    private String ENDPOINT_URL;
 
     public TransactionResponse saveOrder(TransactionRequest request) {
         String response = "";
@@ -26,7 +35,7 @@ public class OrderService {
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
         // rest call
-        Payment paymentResponse = template.postForObject("http://PAYMENT-SERVICE/payment/do-payment", payment, Payment.class);
+        Payment paymentResponse = template.postForObject(ENDPOINT_URL, payment, Payment.class);
 
         assert paymentResponse != null;
         response = paymentResponse.getPaymentStatus().equals("success") ?
